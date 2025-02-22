ada6c0aa6d3547539d675e65f4af4ec9b3fdf754
kazikaif <130026107+kazikaif@users.noreply.github.com>
2025-02-22 16:35:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function updateTime() {
+    const timer = document.getElementById('timer');
+    const now = new Date();
+    const options = { 
+        weekday: 'long', 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric', 
+        hour: '2-digit', 
+        minute: '2-digit', 
+        second: '2-digit',
+        hour12: true
+    };
+    timer.textContent = now.toLocaleString('en-US', options);
+}
+
+setInterval(updateTime, 1000);
+updateTime();
